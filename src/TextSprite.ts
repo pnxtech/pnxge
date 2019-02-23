d8901874ea7427f6564caad7cbd920921c3d6611
cjus <Carlos.Justiniano@gmail.com>
2019-02-23 11:08:15 -0500

    more TextSprite cleanup

--- src/TextSprite.ts
+++ src/TextSprite.ts
@@ -8,6 +8,7 @@ import {Attribs} from './Attribs';
  * @name TextSprite
  * @description extends the PIXI BitmapText to include additional fields
  * @note: uses PIXI BitmapText
+ * @todo: Uses anim simply to hold z value - cleanup later
  */
 export class TextSprite extends PIXI.extras.BitmapText {
   //#region variables
@@ -53,53 +54,6 @@ export class TextSprite extends PIXI.extras.BitmapText {
     this.anim.z = z;
   }
 
-  // /**
-  //  * @name alpha
-  //  * @description alpha getter
-  //  * @return {number} alpha value
-  //  */
-  // public get alpha(): number {
-  //   return (this.anim) ? this.anim.alpha : 1;
-  // }
-
-  // /**
-  //  * @name alpha
-  //  * @description alpha setter
-  //  */
-  // public set alpha(value: number) {
-  //   if (this.anim) {
-  //     this.anim.alpha = value;
-  //   }
-  // }
-
-  /**
-   * @name get Attribs
-   * @description get attributes
-   * @return {Attribs} attributes
-   */
-  public get attribs(): Attribs {
-    return this.attributes;
-  }
-
-  // /**
-  //  * @name visible
-  //  * @description get visibility
-  //  * @return {boolean} true if visible
-  //  */
-  // public get visible(): boolean {
-  //   return (this.anim) ? this.anim.visible : false;
-  // }
-
-  // /**
-  //  * @name visible
-  //  * @description set visibility
-  //  */
-  // public set visible(value: boolean) {
-  //   if (this.anim) {
-  //     this.anim.visible = value;
-  //   }
-  // }
-
   /**
    * @name setTint
    * @description set tint
@@ -108,7 +62,6 @@ export class TextSprite extends PIXI.extras.BitmapText {
    */
   public setTint(color: number):void {
     this.tint = color;
-    // this.anim.setTint(color);
   }
 
   /**
