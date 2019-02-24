946b8157a26c98091730f22342904367b7934436
cjus <Carlos.Justiniano@gmail.com>
2019-02-23 20:01:29 -0500

    fix for logging

--- dist/Scene.js
+++ dist/Scene.js
@@ -277,7 +277,7 @@ var Scene = /** @class */ (function () {
                         continue;
                     }
                     if (obj1.anim.rect.intersect(obj2.anim.rect)) {
-                        console.log("anim " + obj1.currentSequenceName + " collided with " + obj2.currentSequenceName);
+                        console.log("anim " + obj1.anim.currentSequenceName + " collided with " + obj2.anim.currentSequenceName);
                         obj1.anim.onCollision(obj2.anim);
                         obj2.anim.onCollision(obj1.anim);
                     }
